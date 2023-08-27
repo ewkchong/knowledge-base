b407ea1e05750623466f0b272bbd3157419e7c4a
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2023-08-27 14:06:53 -0700

    markdown interpreter WIP

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,37 @@
-import {EditorState, Transaction, Line} from "@codemirror/state"
+import {EditorState, Line} from "@codemirror/state"
 import {EditorView, keymap, placeholder, ViewUpdate, ViewPlugin, Decoration, DecorationSet} from "@codemirror/view"
 import {defaultKeymap} from "@codemirror/commands"
-import { markdown } from "@codemirror/lang-markdown"
+
+function setLineDecoration(decorations: any[], text: string) {
+	if (text.length == 0) return;
+
+	switch (text[0]) {
+		case "#":
+			break;
+		case "":
+			break;
+		default:
+			break;
+	}
+}
+
+function specialMarkdowns(view: EditorView): DecorationSet {
+  let decorations: any[] = []
+  const doc = view.state.doc;
+  for (let lineNum = 1; lineNum <= doc.lines; lineNum++) {
+	 const line: Line = doc.line(lineNum);
+
+	 setLineDecoration(decorations, line.text);
+	 
+     if (line.text && line.text[0] == "#") {
+       let deco = Decoration.line({
+		  class: "cm-h1"
+       })
+	  decorations.push(deco.range(line.from));
+	}
+  }
+  return Decoration.set(decorations)
+}
 
 const markdownPlugin = ViewPlugin.fromClass(class {
   decorations: DecorationSet
@@ -18,26 +48,10 @@ const markdownPlugin = ViewPlugin.fromClass(class {
   decorations: v => v.decorations,
 })
 
-function specialMarkdowns(view: EditorView) {
-  let decorations: any[] = []
-  const doc = view.state.doc;
-  for (let lineNum = 1; lineNum <= doc.lines; lineNum++) {
-	 const line: Line = doc.line(lineNum);
-     if (line.text && line.text[0] == "#") {
-       let deco = Decoration.line({
-		  class: "cm-h1"
-       })
-	  decorations.push(deco.range(line.from));
-	}
-  }
-  return Decoration.set(decorations)
-}
-
 let startState = EditorState.create({
   doc: "",
   extensions: [
 	  keymap.of(defaultKeymap),
-	  markdown(),
 	  markdownPlugin,
 	  placeholder("Start typing...")
   ]
@@ -47,4 +61,5 @@ let view = new EditorView({
   state: startState,
   parent: document.body
 })
+
 
